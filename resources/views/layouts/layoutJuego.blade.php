0cb07cdc7b8e925a557cba6b01af08b1a7447c91
JuanGF04 <chain1lol@gmail.com>
2019-05-14 11:05:42 +0200

    Database link update

--- resources/views/layouts/layoutJuego.blade.php
+++ resources/views/layouts/layoutJuego.blade.php
@@ -37,6 +37,9 @@
 
           <?php
             $icodgame = $_GET['icodgame'];
+						$nombreusuario = $_SESSION['nombre'];
+
+						$puntos = DB::table('users')->where('name', $nombreusuario)->value('points');
 
             $resultado = DB::table('games')->where('icodgame', $icodgame)->get();
 
@@ -49,9 +52,8 @@
 							$genero = $mostrar->genero;
 							$trailer = $mostrar->trailer;
 							$descripcion = $mostrar->descripcion;
-
             }
-
+						echo "$puntos";
 						echo "<img src='$caratula'/>";
 						echo "<div id='infojuego'>";
 						echo "<h1>".$nombre."</h1>";
@@ -59,7 +61,7 @@
 						echo "<h3>Desarrollador: ".$desarrollador."</h3> ";
 						echo "<p>".$descripcion."</p>";
 						echo "<h3>Trailer:</h3>";
-						echo"<video width='700' height= '500' poster='img/jugger.png' controls muted autoplay loop />
+						echo"<video poster='img/jugger.png' controls muted autoplay loop />
 							<source src='$trailer' type='video/mp4' />";
 						echo "</div>";
            ?>
