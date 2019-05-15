45be90051aefd00f87e3962879680a54571d2060
Chain <chain1lol@gmail.com>
2019-05-15 14:10:29 +0200

    Back link in register page

--- resources/views/layouts/layoutRegistro.blade.php
+++ resources/views/layouts/layoutRegistro.blade.php
@@ -2,9 +2,23 @@
 	<head>
 @include("layouts.code.links")
 @yield("links")
+	<style type="text/css">
+		h1 a{
+			color:white;
+			text-decoration: none;
+			transition: .2s all ease-in-out;
+		}
+		h1 a:hover{
+			color:rgba(200,0,0,1);
+			text-decoration: none;
+			transition: .2s all ease-in-out;
+		}
+	</style>
 	</head>
+
+
 	<body>
-		<h1 style="font-family: graceland; color: white;">Juggernaut | Formulario de registro</h1>
+		<h1 style="font-family: graceland; color: white;"><a href=".">Juggernaut</a> | Formulario de registro</h1>
 		@include("layouts.code.formulario")
 		@yield("formulario")
 
