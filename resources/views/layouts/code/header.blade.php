f017e0ef4d6e3a81244d79ed03f90e2b0adf3be5
Chain <chain1lol@gmail.com>
2019-05-23 11:44:15 +0200

    Marquee

--- resources/views/layouts/code/header.blade.php
+++ resources/views/layouts/code/header.blade.php
@@ -1,7 +1,7 @@
-<div class="cabecera">
-  <marquee>Un héroe es todo aquel que hace lo que puede</marquee>
+<!--<div class="cabecera">
+  <span>Un héroe es todo aquel que hace lo que puede</span>
 </div>
-
+-->
 <header class="principal">
   <div>
     <img src="img/juggerizq.png" class="izq"><a href="http://localhost/juggernaut/public/"><h1>Juggernau<span style="font-family:elefont;">7</span></h1></a><img src="img/jugger.png">
