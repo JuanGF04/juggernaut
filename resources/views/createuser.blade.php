1b467e7f7baf629e9cb0a6cab706b42f5c28bbf0
JuanGF04 <chain1lol@gmail.com>
2019-05-27 16:19:57 +0200

    Register form validation

--- resources/views/createuser.blade.php
+++ resources/views/createuser.blade.php
@@ -1,12 +1,29 @@
 <?php
+  session_start();
+
+  $existsname = DB::table('users')->where('name', $_GET['nombre'])->value('icoduser');
+
+  $_SESSION['malname'] = false;
+  $_SESSION['malpass'] = false;
+  $_SESSION['malcorreo'] = false;
 
   if(!isset($_GET['nombre']) || !isset($_GET['pass']) || !isset($_GET['repass'])|| !isset($_GET['mail'])){
     echo "No se reciben las cajas de parámetros";
   }
-  else if($_GET['pass'] != $_GET['repass']){
-		echo "Las contraseñas no coinciden<br/>";
-	}else{
+  if($_GET['nombre'] == '' || $existsname != null){
+    $_SESSION['malname'] = true;
+  }
+  if($_GET['pass'] != $_GET['repass']){
+    $_SESSION['malpass'] = true;
+  }
+  if(!filter_var($_GET['mail'], FILTER_VALIDATE_EMAIL)){
+    $_SESSION['malcorreo'] = true;
+  }
 
+  if($_SESSION['malname'] == true || $_SESSION['malpass'] == true || $_SESSION['malcorreo'] == true ){
+    header('Location: http://localhost/juggernaut/public/registro');
+    die();
+  }else{
     include("salt.inc.php");
 
     $name = $_GET['nombre'];
@@ -16,7 +33,8 @@
     DB::insert('insert into users (icoduser, name, password , mail, points)
     values (?, ?, ?, ?, ?)', array(0, $name, $password, $mail, 0));
 
-    header('Location: http://localhost/juggernaut/public');
+    $getparameters = $name.'&&password='.$_GET['pass'];
+    header("Location: http://localhost/juggernaut/public/login?username=$getparameters");
     die();
   }
 
--- resources/views/layouts/code/formulario.blade.php
+++ resources/views/layouts/code/formulario.blade.php
@@ -1,80 +1,55 @@
+<?php
+session_start();
+//Mostramos los errores al usuario
+if(isset($_SESSION['malname'])){
+  if($_SESSION['malname']==true){
+    echo "<div class='alert alert-danger mt-2' role='alert'>
+      Nombre no disponible.
+    </div>";
+  }
+}
+  if(isset($_SESSION['malpass'])){
+    if($_SESSION['malpass']==true){
+      echo "<div class='alert alert-danger mt-2' role='alert'>
+        Las contraseñas no coinciden.
+      </div>";
+    }
+  }
+
+  if(isset($_SESSION['malcorreo'])){
+  if($_SESSION['malcorreo']==true){
+    echo "<div class='alert alert-danger mt-2' role='alert'>
+      El formato de correo no es válido, prueba ejemplo@ejemplo.com .
+    </div>";
+    
+  }
+}
+
+ ?>
+
+
+
 <div id="formReg">
 <form class="needs-validation" novalidate method="get" action="http://localhost/juggernaut/public/createuser">
   <div class="form-group">
     <div class="col-sm-12">
       <label for="name" id="labelForm">Nombre</label>
       <input class="form-control" type="text" name="nombre" required/><br/>
-      <small class="text-muted">
-        Nombre único identificativo.
-      </small>
-      <div class="invalid-feedback">
-          Lo sentimos, este usuario no está disponible.
-      </div>
-      <div class="valid-feedback">
-          ¡Usuario disponible!
-      </div>
     </div>
 
     <div class="col-sm-12 mt-3">
       <label for="name" id="labelForm">Contraseña</label>
       <input class="form-control" type="password" name="pass" required/><br/>
-      <small class="text-muted">
-        Mínimo 6 caracteres.
-      </small>
-      <div class="invalid-feedback">
-          Error, contraseña incorrecta.
-      </div>
-      <div class="valid-feedback">
-          ¡Contraseña correcta!
-      </div>
     </div>
     <div class="col-sm-12 mt-3">
       <label for="name" id="labelForm">Confirma contraseña</label>
       <input class="form-control" type="password" name="repass" required/><br/>
-      <small class="text-muted">
-        Repite la contraseña.
-      </small>
-      <div class="invalid-feedback">
-          Error, las contraseñas no coinciden.
-      </div>
-      <div class="valid-feedback">
-          ¡Las contraseñas coinciden!
-      </div>
     </div>
     <div class="col-sm-12 mt-3">
       <label for="name" id="labelForm">Email</label>
       <input class="form-control" type="email" name="mail" required/><br/>
-      <small class="text-muted">
-        Formato email, por ejemplo ejemplo@ejemplo.com
-      </small>
-      <div class="invalid-feedback">
-          Error,email incorrecto.
-      </div>
-      <div class="valid-feedback">
-          ¡Email correcto!
-      </div>
     </div>
   <button class="ml-3 mt-3 btn btn-outline-light" type="submit">Registro</button>
   </div>
-  <script>
-  // Example starter JavaScript for disabling form submissions if there are invalid fields
-  (function() {
-    'use strict';
-    window.addEventListener('load', function() {
-
-      var forms = document.getElementsByClassName('needs-validation');
-
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
-  </script>
 </form>
 </div>
